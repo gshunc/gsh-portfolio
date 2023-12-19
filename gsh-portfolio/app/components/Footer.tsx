e521a61458eb7288edb98c1caa9d353bced1305e
George Harris <gsh@ad.unc.edu>
2023-12-19 16:57:51 -0600

    trying to fix error

--- gsh-portfolio/app/components/Footer.tsx
+++ gsh-portfolio/app/components/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <div className="border-t-2 h-8 mb-4 w-screen">
       <div className="text-base ml-5 mt-2">
         {`Written by George Harris. Chesterfield, Missouri, United States of
-        America. 2023. ❤️ 🐏`}
+        America. 2023. ❤️`}
       </div>
     </div>
   );
