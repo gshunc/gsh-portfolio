55c6ff7c623acd553d4fab65953e03bfb02282d9
George Harris <gsh@ad.unc.edu>
2024-02-06 10:58:55 -0500

    fixing file path

--- app/pages/projects_page/page.tsx
+++ app/pages/projects_page/page.tsx
@@ -12,6 +12,14 @@ const projects_page = () => {
       <p className="text-2xl lg:text-4xl underline mt-5">
         Projects and Pursuits
       </p>
+      <p className="text-xl mt-5 lg:text-2xl lg:mt-10">
+        -&gt; How Things Started
+      </p>
+      <p className="mt-5">
+        {
+          "I was born October 15th, 2003 in St. Louis, Missouri to two loving parents. I attended King of Kings Preschool, followed by River Bend Elementary, Pierremont Elementary MOSAIC Gifted Academy, and Parkway Central Middle and High School. In High School, I was involved in a number of organizations, serving as Senior Class President and Varsity Swim Captain, as well as Secretary of Coding Colts. I had the opportunity to give our Graduation Speech, pictured below."
+        }
+      </p>
     </main>
   );
 };
